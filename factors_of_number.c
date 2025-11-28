1e26b7c95a12a7ec853806433a6f7b97cd315513
Naitik <danginaitikk@gmail.com>
2025-11-28 17:53:54 +0530

    Create factors_of_number.c

---
+++ factors_of_number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+    int num;
+    
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    
+    for(int i = 1; i <= num; i++) {
+        if(num % i == 0) {
+            printf("%d\n", i);
+        }
+    }
+    
+    return 0;
+}

062eff85dcd36f61687c51b6814e5e568b3c8fc8
Naitik <danginaitikk@gmail.com>
2025-12-02 18:17:40 +0530

    Add program to calculate sum of first n numbers

---
+++ 06-loops/sum_of_n_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    
+    for(int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    
+    printf("Sum of 1 to %d is: %d\n", n, sum);
+    return 0;
+}

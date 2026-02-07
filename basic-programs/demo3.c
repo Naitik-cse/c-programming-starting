4513014e0874230669d8c1f4f8639367786ed2a9
Naitik <danginaitikk@gmail.com>
2026-02-07 19:29:20 +0530

    Add demo3.c - Multiple C programs demonstrating basic features

---
+++ basic-programs/demo3.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<math.h>
+
+// Program 1: Print dashes
+int main()
+{
+    printf("------ ------\n");
+    return 0;
+}
+
+// Program 2: Print variable values
+int main()
+{
+    int a = 10, b = 20;
+    printf("the value of x is %d y is %d\n", a, b);
+    return 0;
+}
+
+// Program 3: Sum of two numbers
+int main()
+{
+    int a = 20, b = 30, c = a + b;
+    printf("sum of %d and %d is %d\n", a, b, c);
+    return 0;
+}
+
+// Program 4: Print multiple values
+int main()
+{
+    int a = 1, b = 2, c = 3, d = 4, e = 5;
+    printf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n", a, b, b, c, c, c, d, d, d, d, e, e, e, e, e);
+    return 0;
+}
+
+// Program 5: Float demonstration
+int main()
+{
+    float a = 5.10;
+    printf("my height is %f\n", a);
+    return 0;
+}
+
+// Program 6: Item calculation
+int main()
+{
+    float a = 25.5, b = 30.75, c = a + b;
+    printf("Item 1 %f 2 %f %f\n", a, b, c);
+    return 0;
+}
+
+// Program 7: Character and integer
+int main()
+{
+    char a = 'N';
+    printf("my name is start by %c\n", a);
+    return 0;
+}
+
+// Program 8: String
+int main()
+{
+    char a[] = "hello world";
+    printf("%s\n", a);
+    return 0;
+}
+
+// Program 9: Sum with sqrt
+int main()
+{
+    printf("%f\n", sqrt(4));
+    return 0;
+}

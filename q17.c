a50b39b7bd22ffa32f6476ebb6c25d4717ba8126
Naitik <danginaitikk@gmail.com>
2026-02-07 19:44:27 +0530

    Add q17.c - Array element deletion at position
    This program allows the user to delete an element from an array based on a specified position.

---
+++ q17.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main(){
+  int num, pos;
+  printf("Enter number of elements\n");
+  scanf("%d", &num);
+  
+  int arr[num];
+  printf("Enter array elements\n");
+  
+  for(int i = 0; i < num; i++){
+    scanf("%d", &arr[i]);
+  }
+  
+  printf("Array before deletion\n");
+  for(int i = 0; i < num; i++){
+    printf("%d , ", arr[i]);
+  }
+  
+  printf("\nEnter position to delete\n");
+  scanf("%d", &pos);
+  
+  if(pos < 1 || pos > num){
+    printf("Invalid position\n");
+    return 0;
+  }
+  
+  for(int i = pos - 1; i < num - 1; i++){
+    arr[i] = arr[i + 1];
+  }
+  
+  num--;
+  
+  printf("Array after deletion\n");
+  for(int i = 0; i < num; i++){
+    printf("%d , ", arr[i]);
+  }
+  
+  return 0;
+}

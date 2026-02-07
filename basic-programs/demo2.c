7fef66346042c9558e06032503888ef68233f518
Naitik <danginaitikk@gmail.com>
2026-02-07 19:27:28 +0530

    Add demo2.c - Sum and marks calculation programs
    The file contains two programs: one for summing two numbers and another for calculating total marks from three subjects.

---
+++ basic-programs/demo2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+// Program 1: Sum of two numbers
+int main()
+{
+    int no1, no2;
+    printf("give me the no. for sum and\n");
+    scanf("%d", &no1);
+    return 0;
+}
+
+// Program 2: Marks calculation
+int main()
+{
+    int marks1, marks2, marks3, total;
+    printf("enter value of marks1\n");
+    scanf("%d", &marks1);
+    printf("enter value of marks2\n");
+    scanf("%d", &marks2);
+    printf("enter value of marks3\n");
+    scanf("%d", &marks3);
+    total = marks1 + marks2 + marks3;
+    return 0;
+}

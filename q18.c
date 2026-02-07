eb82c6d8f398fdbbfc8e4efd9e6b9e6bd380d363
Naitik <danginaitikk@gmail.com>
2026-02-07 19:45:03 +0530

    Add q18.c - Duplicate elements finder in array
    Implement a program to find duplicate elements in an array.

---
+++ q18.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main(){
+  int n, i, j, count, found = 0;
+  printf("Enter number of elements\n");
+  scanf("%d", &n);
+  
+  if(n <= 0){
+    printf("Invalid size!\n");
+    return 0;
+  }
+  
+  int arr[n];
+  printf("Enter array elements\n");
+  
+  for(i = 0; i < n; i++){
+    scanf("%d", &arr[i]);
+  }
+  
+  printf("Duplicate elements are\n");
+  
+  for(i = 0; i < n; i++){
+    count = 1;
+    if(arr[i] == -1)
+      continue;
+    
+    for(j = i + 1; j < n; j++){
+      if(arr[i] == arr[j]){
+        count++;
+        arr[j] = -1;  // mark duplicate
+      }
+    }
+    
+    if(count > 1){
+      printf("%d , ", arr[i]);
+      found = 1;
+    }
+  }
+  
+  if(!found)
+    printf("-1\n");
+  
+  return 0;
+}

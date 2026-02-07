b04c379d0aca60367c6e77b8c6266e980c82cdda
Naitik <danginaitikk@gmail.com>
2026-02-07 19:33:48 +0530

    Add Fibonacci sequence program in C

---
+++ programs/q8-fibonacci.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 0, b = 1, c;
+    int num;
+    
+    printf("enter the number of terms\n");
+    scanf("%d", &num);
+    
+    for(int i = 1; i <= num; i++)
+    {
+        printf("%d, ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    
+    return 0;
+}
